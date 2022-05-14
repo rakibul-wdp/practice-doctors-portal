3f87f7ac9b9658217d064a8d1e43a8b488da0072
rakibul-wdp <rakibul.wdp@gmail.com>
2022-05-14 11:24:25 +0600

    add navbar section on the header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
+import About from './Pages/About/About';
+import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
+import Navbar from './Pages/Shared/Navbar';
+
 function App() {
   return (
-    <div className='App'>
-      <button class='btn btn-primary'>Button</button>
+    <div className='px-12'>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
     </div>
   );
 }
