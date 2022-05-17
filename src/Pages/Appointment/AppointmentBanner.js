5e9fba96defbff930e7385e1dbddbd799811d904
rakibul-wdp <rakibul.wdp@gmail.com>
2022-05-17 15:04:40 +0600

    add calendar from react day picker and add in appointment page

---
+++ src/Pages/Appointment/AppointmentBanner.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import chair from '../../assets/images/chair.png';
+import background from '../../assets/images/bg.png';
+import { DayPicker } from 'react-day-picker';
+import 'react-day-picker/dist/style.css';
+import { format } from 'date-fns';
+
+const AppointmentBanner = () => {
+  const [date, setDate] = useState(new Date());
+  return (
+    <div className='hero min-h-[70vh]' style={{ backgroundImage: `url(${background})` }}>
+      <div className='hero-content flex-col lg:flex-row-reverse'>
+        <img src={chair} className='max-w-sm rounded-lg shadow-2xl' alt='Dentist patient chair' />
+        <div className='mr-16'>
+          <DayPicker mode='single' selected={date} onSelect={setDate} />
+          <p>You have selected: {format(date, 'PP')}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AppointmentBanner;
